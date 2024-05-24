917ea08a332770267e36320d84480b502d2ff19d
Vero <veronicacamarzana@hotmail.com>
2024-05-24 15:08:07 +0200

    Vue exercices

--- vue/pages/index.vue
+++ vue/pages/index.vue
@@ -3,4 +3,13 @@
     <TodoList />
     <TodoForm />
   </div>
+  <footer>
+    <div class="foot">· Made with Vue, Typescript, Nuxt and Pinia ·</div>
+  </footer>
 </template>
+<style scoped>
+.foot {
+  margin-top: 10px;
+  color: rgb(99, 166, 204);
+}
+</style>
