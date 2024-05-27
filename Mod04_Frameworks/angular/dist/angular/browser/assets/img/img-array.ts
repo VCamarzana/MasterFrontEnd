d10e35ac77941c9952457712d35089446f35a7f5
Vero <veronicacamarzana@hotmail.com>
2024-05-27 12:28:53 +0200

    dist file added

---
+++ Mod04_Frameworks/angular/dist/angular/browser/assets/img/img-array.ts
@@ -0,0 +1,52 @@
+export const imagesArray = [
+  {
+    id: 1,
+    src: 'assets/img/1.jpg',
+    title: 'Airplane',
+  },
+  {
+    id: 2,
+    src: 'assets/img/2.jpg',
+    title: 'Groundhog Day',
+  },
+  {
+    id: 3,
+    src: 'assets/img/3.jpg',
+    title: 'Life Of Brian',
+  },
+  {
+    id: 4,
+    src: 'assets/img/4.jpg',
+    title: 'Shaun Of The Dead',
+  },
+  {
+    id: 5,
+    src: 'assets/img/5.jpg',
+    title: 'Some Like Hot',
+  },
+  {
+    id: 6,
+    src: 'assets/img/6.jpg',
+    title: 'Spaceballs',
+  },
+  {
+    id: 7,
+    src: 'assets/img/7.jpg',
+    title: 'The Holy Grail',
+  },
+  {
+    id: 8,
+    src: 'assets/img/8.jpg',
+    title: 'Top Secret',
+  },
+  {
+    id: 9,
+    src: 'assets/img/9.jpg',
+    title: 'What We Do In The Shadows',
+  },
+  {
+    id: 10,
+    src: 'assets/img/10.jpg',
+    title: 'Young Frankenstein',
+  },
+];
